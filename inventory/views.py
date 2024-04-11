9f1569268a09a30fdc825b156961246eefcdea0b
Ravi Maurya <lcs2020043@iiitl.ac.in>
2024-04-11 23:16:02 +0530

    create view to return inventory by sku

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from inventory.models import Inventory
+from inventory.repostories.inventory_repository import InventoryRepository
+from inventory.serializers import InventorySerializer
 
 # Create your views here.
+
+inventory_repository = InventoryRepository()
+
+@api_view(['GET'])
+def get_inventory_by_sku(request,sku):
+    if not sku:
+        return Response({'message': 'SKU parameter is required'},status=status.HTTP_400_BAD_REQUEST)
+    try:
+        inventory = inventory_repository.get_inventory_by_sku(sku)
+        serialized_data = InventorySerializer(inventory).data
+        return Response(serialized_data, status=status.HTTP_200_OK)
+    except Inventory.DoesNotExist:
+        return Response({'message': 'Inventory not found'}, status=status.HTTP_404_NOT_FOUND)
