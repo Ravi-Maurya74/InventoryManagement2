03e599eb7a71280cb83f55a026789b6bfbcfac93
Ravi Maurya <lcs2020043@iiitl.ac.in>
2024-04-11 19:37:50 +0530

    change to dependency injection

--- inventory/services/inventory_service.py
+++ inventory/services/inventory_service.py
@@ -1,8 +1,8 @@
 from inventory.repostories.inventory_repository import InventoryRepository
 
 class InventoryService:
-    def __init__(self) -> None:
-        self.respository = InventoryRepository()
+    def __init__(self,repository:InventoryRepository) -> None:
+        self.respository = repository
 
     def get_inventory_by_sku(self,inventory_sku):
         return self.respository.get_inventory_by_sku(inventory_sku=inventory_sku)
